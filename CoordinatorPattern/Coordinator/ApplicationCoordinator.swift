2396d7cf510b58c999fe1ebf265fee7190e82449
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2019-05-01 19:26:40 +0800

    1.Created KanjiListCoordinator 2. Used KanjiListCoordinator to init the rootViewcontroller in AppDelegate

--- CoordinatorPattern/Coordinator/ApplicationCoordinator.swift
+++ CoordinatorPattern/Coordinator/ApplicationCoordinator.swift
@@ -13,21 +13,27 @@ class ApplicationCoordinator: Coordinator {
     let kanjiStorage: KanjiStorage
     let window: UIWindow
     let rootViewController: UINavigationController
+    let kanjiListCoordinator: KanjiListControllerCoordinator
     
     init(window: UIWindow){
         self.window = window
         kanjiStorage = KanjiStorage()
         rootViewController = UINavigationController()
         rootViewController.navigationBar.prefersLargeTitles = true
+        kanjiListCoordinator = KanjiListControllerCoordinator.init(presenter: rootViewController, kanjiStorage: kanjiStorage)
+        
         
         //Testing Code
+        /*
         let emptyViewController = UIViewController()
         emptyViewController.view.backgroundColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)
         rootViewController.pushViewController(emptyViewController, animated: true)
+        */
     }
     
     func start() {
         window.rootViewController = rootViewController
+        kanjiListCoordinator.start()
         window.makeKeyAndVisible()
     }
     
